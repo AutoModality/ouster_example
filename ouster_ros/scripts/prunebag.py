67d9c1d30b79b920834a7ff515619b01daf79059
Jimi Damon <jdamon@gmail.com>
2020-02-07 16:48:14 -0800

    Updated script for pruning and launch files that assist with characterization feature/PD-16

--- ouster_ros/scripts/prunebag.py
+++ ouster_ros/scripts/prunebag.py
@@ -13,15 +13,31 @@ import math
 import argparse
 
 
-def prune_bag(inbag,outbag,n=100,start=0):
-    with rosbag.Bag(outbag, 'w') as outbagfile:
-        for topic, msg, t in rosbag.Bag(inbag).read_messages():
-            while n:
-                if start <= 0 :
-                    outbagfile.write(topic, msg, t)
-                    n -= 1
-                else:
-                    start -= 1
+def prune_bag(infile,outfile,num_msgs=100,start=0):
+    #import ipdb; ipdb.set_trace()
+    outbag = rosbag.Bag(outfile, 'w')
+    tmp = rosbag.Bag(infile).read_messages()
+
+    while num_msgs:
+        x = tmp.next()
+        if start <= 0:
+            topic = x.topic
+            msg = x.message
+            t = x.timestamp
+            outbag.write(topic, msg, t)
+            num_msgs -= 1
+        else:
+            start -= 1
+
+# print("Reached")
+# with rosbag.Bag(outbag, 'w') as outbagfile: }
+#     for topic, msg, t in rosbag.Bag(inbag).read_messages(): }
+#         while n: }
+#             if start <= 0 : }
+#                 outbagfile.write(topic, msg, t) }
+#                 n -= 1 }
+#             else: }
+#                 start -= 1 }
 
 
 def parse_args():
@@ -35,7 +51,7 @@ def parse_args():
     parser.add_argument("-n","--nummessages",default=100,type=int,
                         help="Number of messages") 
 
-    parser.add_argument("-s","--start_msg",default=0,
+    parser.add_argument("-s","--start",default=0,type=int,
                         help="Message to start on") 
 
     args = parser.parse_args()
@@ -49,5 +65,5 @@ def parse_args():
    
 if __name__ == "__main__":
     args = parse_args()
-    prune_bag(args.inbag,args.outbag,args.nummessages)
+    prune_bag(args.inbag,args.outbag,args.nummessages,args.start)
    
