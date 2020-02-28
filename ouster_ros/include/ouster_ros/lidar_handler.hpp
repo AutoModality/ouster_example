d891c937b00a4167346f5556739a76f9f47cc27f
edkoch <ed@thekochs.net>
2020-02-28 01:55:29 +0000

    BEtter debug statement for inspecting time ranges feature/PD-20

--- ouster_ros/include/ouster_ros/lidar_handler.hpp
+++ ouster_ros/include/ouster_ros/lidar_handler.hpp
@@ -8,9 +8,16 @@
         do { 
           status = CanProcess( cb, imu_buf, 10000 );
 
-          ROS_DEBUG_STREAM_THROTTLE(1,"cb(" << cb.size() << ")" << " imu(" << imu_buf.size() << ")");
-            switch (status) {
+          // ROS_DEBUG_STREAM_THROTTLE(1,"cb(" << cb.size() << ")" << " imu(" << imu_buf.size() << ")");
+	  if ( cb.size() >= 1 && imu_buf.size() >= 1 ) 
+	    ROS_DEBUG_STREAM_THROTTLE(1,
+				      "cb(" << cb.size() << ")" << " imu(" << imu_buf.size() << ")\n" <<
+				      "LidarPkts = [ " << GetTimes((*cb.front()))[0] << " , " << GetTimes((*cb.back()))[15] << " ]\n" << 
+				      "Imus      = [ " << imu_buf.front().header.stamp.toNSec() << " , " << imu_buf.back().header.stamp.toNSec() << " ]\n");
+	  
+	  switch (status) {
                 case LidarStates::PROCESS:
+		  ROS_DEBUG("Processing");
                   tmppm = cb.front();
                   batch_and_publish(tmppm->buf.data(), it , local_scan);
                   cb.pop_front();
